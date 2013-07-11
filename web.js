940d9fbe9eb76d81617b906b9a0252801a92a825
vettipaiyan <snaraya3@gmail.com>
2013-07-11 05:40:44 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync('index.html','utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
